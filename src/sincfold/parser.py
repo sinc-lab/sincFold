e0b062552ffc0e98f16b91a3706d0a1a9531a5a0
lbugnon <lbugnon@sinc.unl.edu.ar>
2023-09-13 01:06:27 -0300

    fix dtype

--- src/sincfold/parser.py
+++ src/sincfold/parser.py
@@ -13,7 +13,7 @@ def parser():
     #parser.add_argument("--use-restrictions", action="store_true", help="Use external restriction matrix (default: False)")
     parser.add_argument("-j", type=int, default=2, help="Number of parallel workers")
     parser.add_argument("--quiet", action="store_true", help="Quiet mode (default: False)") 
-    parser.add_argument("--max-length", default=512, help="Maximum sequence length to process (default: 512") 
+    parser.add_argument("--max-length", type=int, default=512, help="Maximum sequence length to process (default: 512") 
     
 
     subparsers = parser.add_subparsers(
